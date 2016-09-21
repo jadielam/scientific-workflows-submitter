9cd52e75a2368720b2ef87aa0750a71606007333
Jadiel deArmas <Jadiel.X.De.Armas.-ND@disney.com>
2016-09-21 18:42:36 -0400

    FIrst commit adding Maven project structure.

---
+++ src/main/java/io/biblia/workflows/submitter/App.java
@@ -0,0 +1,14 @@
+package io.biblia.workflows.submitter;
+
+import io.biblia.workflows.manager.WorkflowManager;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
